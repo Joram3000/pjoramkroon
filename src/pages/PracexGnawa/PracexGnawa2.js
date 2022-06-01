32d253d469540932560571beb0464cab4d114c00
Joram3000 <98752947+Joram3000@users.noreply.github.com>
2022-06-01 16:14:38 +0200

    dargnawa page

--- src/pages/PracexGnawa/PracexGnawa2.js
+++ src/pages/PracexGnawa/PracexGnawa2.js
@@ -10,17 +10,18 @@ import { Parallax } from "react-scroll-parallax";
 import "./style.css";
 
 import Africadelic_PracexBlackKoyo from "../../assets/GnawaExploration/Africadelic_PracexBlackKoyo.mp4";
-import AtlasElectronic_PracexSattaralSaadiResized from "../../assets/GnawaExploration/AtlasElectronic_PracexSattaralSaadiResized.png";
 import AtlasElectronic_PracexSattaralSaadiResized2 from "../../assets/GnawaExploration/AtlasElectronic_PracexSattaralSaadiResized3.png";
 import AtlasElectronic_2016II from "../../assets/GnawaExploration/AtlasElectronic_2016II.jpeg";
 import AtlasElectronic_2018krkb from "../../assets/GnawaExploration/AtlasElectronic_2018krkb.jpeg";
-import AtlasElectronic_2018krkb2 from "../../assets/GnawaExploration/14324569_532666263592624_4591781360788162571_o.jpeg";
 import AtlasElectronic_2018gimbri from "../../assets/GnawaExploration/14290035_532664846926099_8116276477418946410_o.jpeg";
 import AtlasElectronic_2018zaal from "../../assets/GnawaExploration/14372389_532664233592827_5396893453420820056_o.jpeg";
 
 import Sahara_Prace from "../../assets/GnawaExploration/Sahara_Prace.mp4";
 import DarGnawa_Talking from "../../assets/GnawaExploration/DarGnawa_Talking.jpeg";
 import DarGnawa_Abdoul from "../../assets/GnawaExploration/DarGnawa_Abdoul.jpg";
+
+import DarGnawa_AmazighBlues from "../../assets/GnawaExploration/DarGnawa_AmazighBlues.JPG";
+import DarGnawa_AmazighBluesHark from "../../assets/GnawaExploration/DarGnawa_AmazighBluesHark.JPG";
 
 export default function PracexGnawa() {
   return (
@@ -45,8 +46,6 @@ export default function PracexGnawa() {
         <Parallax className="headertext">
           The<br></br>Gnawa<br></br>Expedition
         </Parallax>
-
-        {/* <div className="soIwent">Sattar and me playing at Atlas Electronic</div> */}
       </div>
 
       <div className="pager">
@@ -55,17 +54,6 @@ export default function PracexGnawa() {
           maar dat staat er niet <br></br>
           oké doei!
         </div>
-
-        {/* <img
-          style={{
-            width: "100%",
-            position: "relative",
-          }}
-          className="bg"
-          id="AtlasElectronic_2018krkb2"
-          src={AtlasElectronic_2018krkb2}
-          alt=""
-        /> */}
 
         <img
           style={{
@@ -127,7 +115,38 @@ export default function PracexGnawa() {
           alt=""
         />
 
-        <div style={{ position: "absolute" }}>hier staat text</div>
+        <div style={{ position: "absolute", zIndex: "12" }}>
+          <p>hier staat text</p>
+        </div>
+
+        <div id="dargnawatextbox">
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          hoi allemaal <br></br> hier zou een heel leuk textkje kunnen staan,
+          maar dat staat er niet <br></br>
+          oké doei! <br></br>
+          hoi allemaal <br></br> hier zou een heel leuk textkje kunnen staan,
+          maar dat staat er niet <br></br>
+          oké doei! <br></br>
+          hoi allemaal <br></br> hier zou een heel leuk textkje kunnen staan,
+          maar dat staat er niet <br></br>
+          oké doei! <br></br>
+          hoi allemaal <br></br> hier zou een heel leuk textkje kunnen staan,
+          maar dat staat er niet <br></br>
+          oké doei! <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+        </div>
+
         <img
           style={{
             width: "100%",
@@ -137,11 +156,41 @@ export default function PracexGnawa() {
           src={DarGnawa_Abdoul}
           alt=""
         />
+      </div>
 
-        <div id="Aetextbox">
-          hoi allemaal <br></br> hier zou een heel leuk textkje kunnen staan,
-          maar dat staat er niet <br></br>
-          oké doei!
+      <div className="pager">
+        <img
+          style={{
+            width: "100%",
+            borderRadius: "10px",
+          }}
+          className="bg"
+          id="DarGnawa_AmazighBlues"
+          src={DarGnawa_AmazighBlues}
+          alt=""
+        />
+        <img
+          style={{
+            width: "100%",
+            borderRadius: "10px",
+          }}
+          className="bg"
+          id="DarGnawa_AmazighBluesHark"
+          src={DarGnawa_AmazighBluesHark}
+          alt=""
+        />
+
+        <div id="embedAmazigh">
+          <iframe
+            title="embedAmazigh"
+            style={{ border: "0", width: "450px", height: "373px" }}
+            src="https://bandcamp.com/EmbeddedPlayer/album=2256435908/size=large/bgcol=333333/linkcol=0f91ff/artwork=small/transparent=true/"
+            seamless
+          >
+            <a href="https://amazighblues.bandcamp.com/album/amazigh-blues">
+              Amazigh Blues by Amazigh Blues
+            </a>
+          </iframe>
         </div>
       </div>
 
